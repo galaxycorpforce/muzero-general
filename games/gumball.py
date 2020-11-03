ba0e9e6c23465ff681928bb430671844e6905e18
galaxycorpforce <60058833+galaxycorpforce@users.noreply.github.com>
2020-11-03 02:28:46 -0500

    Update gumball.py

--- games/gumball.py
+++ games/gumball.py
@@ -30,7 +30,7 @@ class MuZeroConfig:
 
 
         ### Self-Play
-        self.num_workers = 10  # Number of simultaneous threads/workers self-playing to feed the replay buffer
+        self.num_workers = 2  # Number of simultaneous threads/workers self-playing to feed the replay buffer
         self.selfplay_on_gpu = False
         self.max_moves = 42  # Maximum number of moves if game is not finished before
         self.num_simulations = 200  # Number of future moves self-simulated
